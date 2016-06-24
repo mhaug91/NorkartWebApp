9d00b5b865642c107505b25e6a73c770012f507f
Martin Barhaug <mhaug91@gmail.com>
2016-06-24 16:09:03 +0200

    controller test

---
+++ norkartSommerWebApp/norkartSommerWebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace norkartSommerWebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Welcome()
+        {
+            return "hi world";
+        }
+
+        public String Default()
+        {
+            return "default ppage";
+        }
+    }
+}
